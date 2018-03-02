6cbf90260f22a169e748820cde1f6a2f828896d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-02 23:51:41 +0000

    Deploy tokio-rs/tokio to github.com/tokio-rs/tokio.git:gh-pages

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["tokio_executor"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"tokio_executor/struct.Enter.html\" title=\"struct tokio_executor::Enter\">Enter</a>",];
+implementors["tokio_reactor"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"tokio_reactor/struct.Background.html\" title=\"struct tokio_reactor::Background\">Background</a>",];
+implementors["tokio_threadpool"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"tokio_threadpool/struct.Worker.html\" title=\"struct tokio_threadpool::Worker\">Worker</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
